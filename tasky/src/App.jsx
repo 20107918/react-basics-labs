f11e2409634e8fada6d08f722be1b1e5d08f70e8
dw <dwfixe@gmail.com>
2025-10-27 15:36:14 +0000

    Mark task as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -4,24 +4,40 @@ import Task from './components/Task';
 
 function App() {
 
+    const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
+
+
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: 1 },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: 2 },
-      { id: 3, title: "Tidy up", deadline: "Today", priority: 3 }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: 1, done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: 2, done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", priority: 3, done: false }
     ]
   });
+
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
 
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (              
+      {taskState.tasks.map((task, index) => (              
         <Task 
           title={task.title}
           description={task.description}
           deadline={task.deadline}
           key={task.id}
           priority={task.priority}
+          done={task.done}
+          markDone={() => doneHandler(index)}
         />
       ))}
     </div>
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -19,11 +19,12 @@ const Task = (props) => {
     const priorityClass = priorityLabel.toLowerCase();
     
     return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description"> {props.description}</p>
             <p className={`priority-label ${priorityClass}`}>{priorityLabel}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
         </div>
     )
 
